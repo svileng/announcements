9f3fb597f7f304b2acf9ef105e8c1513d8b160ee
Chase Southard <chase.southard@gmail.com>
2012-04-14 13:27:12 -0400

    fixing typo. adding another class to the announcement div

--- lib/announcements/announcements_helper.rb
+++ lib/announcements/announcements_helper.rb
@@ -9,11 +9,11 @@ module AnnouncementsHelper
 	# hide_message -- clickable text which hides the announcement, default is "hide message"
 	def announce announcement, options = {}
 		text = options[:hide_message] || "x"
-		div_class = options[:div_class] || "alert"
+		div_class = options[:div_class] || "alert alert-block"
 		alert_heading = options[:alert_heading] || "Warning!"
 		if announcement != nil && cookies["announcement_" + announcement.id.to_s] != "hidden"
 			content_tag :div, :class => div_class do
-				results = content_tag(:h4, alert_heading, :class => "alert-heading")
+				result = content_tag(:h4, alert_heading, :class => "alert-heading")
 				result << announcement.body.html_safe
 				result << content_tag(:a, text, :class => "close", :data => { :announcementid => announcement.id })
 				result
