2f45f048eaac2500ac4c916b0ac16bfc590b56eb
Svilen Gospodinov <svilen@gospodinov.co.uk>
2012-04-19 19:59:02 +0200

    Fixing tab spacing...

--- lib/announcements/announcements_helper.rb
+++ lib/announcements/announcements_helper.rb
@@ -1,17 +1,17 @@
 module AnnouncementsHelper
 
-	# Outputs given announcement in HTML (with optional twitter bootstrap style).
-	# 
-	# Basic usage: <%= announce Announcement.newest %>
-	# Alt usage: <%= announce Announcement.newest, :format => "boostrap" %>
-	#
-	# Options:
-	#   div_class -- name for a custom div class, which wraps the announcement text and hide message, default is "info"
-	#   hide_message -- clickable text which hides the announcement, default is "hide message"
-	#   format[:boostrap] -- format announcement to use the twitter bootstrap Alert style, defaults to normals stylings.
-	#   alert_heading -- Adding an alert heading when used with the bootstrap option.
-	def announce announcement, options = {}
-		result = nil
+  # Outputs given announcement in HTML (with optional twitter bootstrap style).
+  #
+  # Basic usage: <%= announce Announcement.newest %>
+  # Alt usage: <%= announce Announcement.newest, :format => "boostrap" %>
+  #
+  # Options:
+  # div_class -- name for a custom div class, which wraps the announcement text and hide message, default is "info"
+  # hide_message -- clickable text which hides the announcement, default is "hide message"
+  # format[:boostrap] -- format announcement to use the twitter bootstrap Alert style, defaults to normals stylings.
+  # alert_heading -- Adding an alert heading when used with the bootstrap option.
+  def announce announcement, options = {}
+    result = nil
     data_attribute = { :announcementid => announcement.id }
 
     if announcement != nil && cookies["announcement_" + announcement.id.to_s] != "hidden"
@@ -35,6 +35,6 @@ module AnnouncementsHelper
     end
 
     result
-	end
+  end
 
 end
